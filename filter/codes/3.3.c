6545296b2995811e7535ca6eec439fe4b2c51f9a
Ninad <Ninad-Meshram>
2022-09-28 23:00:03 +0530

    a

--- filter/codes/3.3.c
+++ filter/codes/3.3.c
@@ -1,27 +1,22 @@
-// Generating filter output values and storing them in a file
-
-// Name: Ankit Saha
-// Roll number: AI21BTECH11004
-
 #include <stdio.h>
 
 #define N 20
 
-double x(int n) {
-	if (n < 0 || n > 5) return 0;
-	else if (n < 4) return n + 1;
-	else return 6 - n;
+double x(int i) {
+	if (i < 0 || i > 5) return 0;
+	else if (i < 4) return i + 1;
+	else return 6 - i;
 }
 
-double y(int n) {
-	if (n < 0) return 0;
-	else return x(n) + x(n-2) - 0.5 * y(n-1);
+double y(int i) {
+	if (i < 0) return 0;
+	else return x(i) + x(i-2) - 0.5 * y(i-1);
 }
 
 int main() {
 	FILE *fp = fopen("filter_output.dat", "w");
-	for (int i = 0; i < N; i++) {
-		fprintf(fp, "%lf\n", y(i));
+	for (int a = 0; a< N; a++) {
+		fprintf(fp, "%lf\n", y(a));
 	}
 	fclose(fp);
 	return 0;
