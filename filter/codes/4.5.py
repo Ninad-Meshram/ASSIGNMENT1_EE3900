93044082e8a251e43242fea9bbc5b576a6824f15
Ninad <Ninad-Meshram>
2022-09-28 23:23:44 +0530

    a

--- filter/codes/4.5.py
+++ filter/codes/4.5.py
@@ -10,8 +10,7 @@ def H2(w):
 Omega1 = np.linspace(-3.5*np.pi, 3.5*np.pi, 80)
 Omega2 = np.linspace(-3.5*np.pi, 3.5*np.pi, 500)
 
-plt.plot(Omega2, H2(Omega2), label='$\\frac{4|\cos\omega|}{\sqrt{5 + 4\cos\omega}}$')
-plt.plot(Omega1, np.abs(H1(np.exp(1j * Omega1))), 'o', label='$\\frac{|1 + e^{-2\jmath\omega}|}{|1 + \\frac{1}{2} e^{-\jmath\omega}|}$')
+
 plt.title('Filter Frequency Response')
 plt.xlabel('$\omega$')
 plt.ylabel('$|H(e^{\jmath\omega})| $')
